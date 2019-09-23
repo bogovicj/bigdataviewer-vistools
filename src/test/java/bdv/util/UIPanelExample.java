ac5a41032ca8f65937bc5e86059af0debccfeea9
tibuch <tibuch@mpi-cbg.de>
2019-09-23 15:56:37 +0200

    Add overlay

--- src/test/java/bdv/util/UIPanelExample.java
+++ src/test/java/bdv/util/UIPanelExample.java
@@ -2,14 +2,19 @@ package bdv.util;
 
 import bdv.util.uipanel.BdvUIPanel;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import net.imglib2.RealPoint;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.FloatArray;
 import net.imglib2.img.basictypeaccess.array.IntArray;
+import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.real.FloatType;
 
@@ -34,7 +39,44 @@ public class UIPanelExample
 		frame.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
 		frame.setVisible( true );
 
+		final BdvOverlay overlay = createBdvOverlay( random );
+
+		BdvFunctions.showOverlay( overlay, "overlay", Bdv.options().addTo( bdv ).axisOrder( AxisOrder.XY ) );
+
 		BdvFunctions.show( img, "img", Bdv.options().addTo( bdv ).axisOrder( AxisOrder.XYC ) );
 		BdvFunctions.show( img_float, "test", Bdv.options().addTo( bdv ).axisOrder( AxisOrder.XY ) );
+	}
+
+	private static BdvOverlay createBdvOverlay( final Random random )
+	{
+		final ArrayList< RealPoint > points = new ArrayList<>();
+		for ( int i = 0; i < 1100; ++i )
+			points.add( new RealPoint( random.nextInt( 100 ), random.nextInt( 100 ) ) );
+
+		return new BdvOverlay()
+		{
+			@Override
+			protected void draw( final Graphics2D g )
+			{
+				final AffineTransform2D t = new AffineTransform2D();
+				getCurrentTransform2D( t );
+
+				g.setColor( Color.RED );
+
+				final double[] lPos = new double[ 2 ];
+				final double[] gPos1 = new double[ 2 ];
+				final double[] gPos2 = new double[ 2 ];
+				final int start = info.getTimePointIndex() * 10;
+				final int end = info.getTimePointIndex() * 10 + 100;
+				for ( int i = start; i < end; i+=2 )
+				{
+					points.get( i ).localize( lPos );
+					t.apply( lPos, gPos1 );
+					points.get( i + 1 ).localize( lPos );
+					t.apply( lPos, gPos2 );
+					g.drawLine( ( int ) gPos1[ 0 ], ( int ) gPos1[ 1 ], ( int ) gPos2[ 0 ], ( int ) gPos2[ 1 ] );
+				}
+			}
+		};
 	}
 }
